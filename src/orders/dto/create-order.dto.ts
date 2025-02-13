5e5f7c059ea39517f15f70981f363e1cec7df8ff
Carles <carmar85@gmail.com>
2025-02-13 12:09:51 +0100

    add order item

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,13 +1,15 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+
+  /*
   @IsNumber()
   @IsPositive()
   totalAmount: number;
@@ -25,4 +27,5 @@ export class CreateOrderDto {
   @IsBoolean()
   @IsOptional()
   paid: boolean = false;
+  */
 }
