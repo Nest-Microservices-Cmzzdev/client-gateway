2c5d15c9db9af7ce3479b51b68f270dd987a0e61
Carles <carmar85@gmail.com>
2025-02-14 18:48:15 +0100

    connect to nats server

--- README.md
+++ README.md
@@ -11,14 +11,15 @@ The gateway is the communication point between our clients and our services. It 
 1. Clone rep
 2. Install dependencies
 3. Create `.env` file based on `env.example`
-4. Have launched the microservices to be consumed
-5. ```
-   docker compose up -d
-   ```
-6. Exec `npm run start:dev`
-
-## Nats
+4. Have launched NATS server
 
 ```
 docker run -d --name nats-server -p 4222:4222 -p 8222:8222 nats
 ```
+
+5. Have launched the microservices to be consumed
+
+6. ```
+   docker compose up -d
+   ```
+7. Exec `npm run start:dev`
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,33 +10,30 @@ import {
   Patch,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
-  @Inject(ORDER_SERVICE)
-  private readonly ordersClient: ClientProxy;
+  @Inject(NATS_SERVICE)
+  private readonly client: ClientProxy;
   constructor() {}
 
   @Post()
   createOrder(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send({ cmd: 'create_order' }, createOrderDto);
+    return this.client.send({ cmd: 'create_order' }, createOrderDto);
   }
 
   @Get()
   findAllOrders(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient.send(
-      { cmd: 'find_all_orders' },
-      orderPaginationDto,
-    );
+    return this.client.send({ cmd: 'find_all_orders' }, orderPaginationDto);
   }
 
   @Get('id/:id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.ordersClient.send({ cmd: 'find_one_order' }, { id }).pipe(
+    return this.client.send({ cmd: 'find_one_order' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -49,7 +46,7 @@ export class OrdersController {
     @Param() statusDto: StatusDto,
     @Query() paginationDto: PaginationDto,
   ) {
-    return this.ordersClient
+    return this.client
       .send(
         { cmd: 'find_all_orders' },
         { ...paginationDto, status: statusDto.status },
@@ -66,7 +63,7 @@ export class OrdersController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() statusDto: StatusDto,
   ) {
-    return this.ordersClient
+    return this.client
       .send({ cmd: 'change_order_status' }, { id, status: statusDto.status })
       .pipe(
         catchError((err) => {
