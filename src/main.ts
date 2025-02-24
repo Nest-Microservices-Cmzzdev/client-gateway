fd17429a0ab326f2fc9e50e24ca12e71253246a4
Carles <carmar85@gmail.com>
2025-02-24 11:14:30 +0100

    type first change - test

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
+
+  console.log('HelloWorld - first change');
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
